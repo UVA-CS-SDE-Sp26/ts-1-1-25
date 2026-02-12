90c43d7701f04f96f6a4fd374206f31612655200
Styx <styx@styxd.dev>
2026-02-12 13:33:56 -0500

    Add main method.

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -2,4 +2,13 @@
  * Commmand Line Utility
  */
 public class TopSecret {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        try {
+            String output = userInterface.parseArguments(args);
+            System.out.println(output);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
